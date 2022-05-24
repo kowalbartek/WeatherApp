e5ac8a0513f89c05d12c40ca95bbd8cd40ac5522
Bartek Kowal <93825166+kowalbartek@users.noreply.github.com>
2022-05-24 21:57:29 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+let weather = {
+  apikey: "c7dd1f48d200b1c6bc7c0594a4501c5e",
+  fetchWeather: function (city) {
+    fetch(
+        // Template link to fetch data from specified city using API
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apikey}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    // Constants made from JSON
+    const { name } = data,
+    { description, icon } = data.weather[0],
+    { temp, humidity } = data.main,
+    { speed } = data.wind;
+    // console.log(name, icon, description, temp, humidity, speed);
+
+    // Show searched location
+    document.querySelector(".location-city").innerHTML = "Weather in " + name;
+    // Show icon relevant to weather
+    document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    // Show temperature
+    document.querySelector(".temperature-degree").innerHTML = temp + "°C";
+    // Show humidity
+    document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+    // Show wind
+    document.querySelector(".wind").innerHTML = "Wind: " + speed + "Km/hr";
+    // Remove "loading"
+    document.querySelector(".weather ").classList.remove("loading");
+    // Set the background to image of the city entered
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/1920x1080/?${description}')`;
+    // Reset search bar value to empty
+    document.querySelector(".search-bar").value = "";
+  },
+  search: function () {
+    // Pointer to value entered
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+// On click of button, search
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+// On key presses Enter. search
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      weather.search();
+    }
+  });
+
+// Default town, Maynooth, my universities town
+weather.fetchWeather("Maynooth");
